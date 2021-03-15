f773f513abe941785452480f07116237cba102e2
Hernán Vargas <hern.var@gmail.com>
2021-03-15 19:51:55 -0300

    Travis

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -xe
+SOURCE_DIR=$1
+DEST_DIR=$2
+
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+echo "Tag: $TRAVIS_TAG"
+echo "Branch:  $TRAVIS_BRANCH"
+echo "Moving from $SOURCE_DIR to $DEST_DIR"
+rsync -r --delete-after $SOURCE_DIR  mintui@mint.isi.edu:$DEST_DIR
+rsync -r --delete-after $SOURCE_DIR  mintui@54.148.90.74:$DEST_DIR
+exit $?	
